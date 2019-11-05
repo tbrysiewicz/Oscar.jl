cb3f52a8baa3ddeaa3b506cd7570b60edabf207c
autodocs <autodocs>
2019-11-05 16:14:33 +0000

    build based on 397d8e2

--- latest/search_index.js
+++ latest/search_index.js
@@ -1,6 +1,22 @@
 var documenterSearchIndex = {"docs": [
 
 {
+    "location": "index.html#",
+    "page": "Welcome to Oscar",
+    "title": "Welcome to Oscar",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Welcome-to-Oscar-1",
+    "page": "Welcome to Oscar",
+    "title": "Welcome to Oscar",
+    "category": "section",
+    "text": "Oscar is a new computer algebra system under development."
+},
+
+{
     "location": "integer.html#",
     "page": "Integers",
     "title": "Integers",
