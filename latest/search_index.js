43f50b1f1fe0714e1e205b974e84dfcb97b7dbdf
autodocs <autodocs>
2019-11-05 14:47:52 +0000

    build based on 075382b

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "integer.html#",
+    "page": "Integers",
+    "title": "Integers",
+    "category": "page",
+    "text": "CurrentModule = Oscar"
+},
+
+{
+    "location": "integer.html#Integers-1",
+    "page": "Integers",
+    "title": "Integers",
+    "category": "section",
+    "text": "This is a test."
+},
+
+]}
