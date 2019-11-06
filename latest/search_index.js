2bf838189fe07beb957aa50c07398d51e26cd0be
autodocs <autodocs>
2019-11-06 15:33:14 +0000

    build based on a8d5cc3

--- latest/search_index.js
+++ latest/search_index.js
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Integers",
     "title": "Integers",
     "category": "section",
-    "text": "This is a test."
+    "text": "An important design decision in Oscar.jl is to use Julia as the user language by default. This means that integers typed at the REPL are Julia integers.For performance reasons, Oscar has its own integer format. These are entered using the ZZ constructor.a = ZZ(2)^100For convenience, many Oscar functions also accept Julia integers as inputs by converting them to Oscar integers, especially if they do not fit in a machine word. For example:R, x = ZZ[\"x\"] # create a polynomial ring over the integers\nf = 2xIn this example, 2 is a Julia integer but is still valid in the call to the Oscar polynomial multiplication function that is implicit in the expression 2x.note: Note\nIn Julia, `2^64` will return `0`, as the Julia integer `2` is a machine\nword. In Oscar, the expression `ZZ(2)^64` will return the correct\nresult (as an Oscar integer). In general, Oscar can only do an\nautomatic conversion to an Oscar integer if the Julia integer is\ncombined with another Oscar expression, or passed to an Oscar function."
 },
 
 ]}
